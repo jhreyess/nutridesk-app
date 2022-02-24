bf167e5841f455c6d46e58231f5ca338c101a65c
Josué Reyes <josue.hreyess@gmail.com>
2022-02-24 01:42:05 -0600

    Adds conditional rendering based on roles

--- app/src/main/java/com/nutrikares/nutrideskapp/SignIn.kt
+++ app/src/main/java/com/nutrikares/nutrideskapp/SignIn.kt
@@ -5,14 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
+import android.widget.Toast
 import com.nutrikares.nutrideskapp.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.nutrikares.nutrideskapp.models.User
 
 class SignIn : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,7 @@ class SignIn : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
+        database = Firebase.database.reference
 
     }
 
@@ -37,6 +47,29 @@ class SignIn : AppCompatActivity() {
     }
 
     private fun launchApp(){
-        startActivity(Intent(this, MainActivity::class.java))
+        val userId = auth.currentUser?.uid.toString()
+        if (!TextUtils.isEmpty(userId)){
+
+            lateinit var userInfo: User
+            val queryRef = database.child("users").child(userId)
+            val listener = object : ValueEventListener {
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                       userInfo = snapshot.getValue(User::class.java)!!
+                        val activity = when(userInfo.getRole()){
+                            "admin" -> AdminMainActivity::class.java
+                            else -> MainActivity::class.java
+                        }
+
+                        startActivity(Intent(this@SignIn, activity))
+                    }
+
+                    override fun onCancelled(err: DatabaseError) {
+                        Toast.makeText(this@SignIn,"Something went wrong!",Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            queryRef.addListenerForSingleValueEvent(listener)
+
+        }
     }
 }
---
+++ app/src/main/java/com/nutrikares/nutrideskapp/models/User.kt
@@ -0,0 +1,10 @@
+package com.nutrikares.nutrideskapp.models
+
+class User() {
+    private lateinit var name: String
+    private lateinit var role: String
+
+    fun getName() = name
+    fun getRole() = role
+
+}
