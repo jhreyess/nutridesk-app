b92f1b4a475113e76fcce7d0d95ccd2f229d27a9
DiegoAlvarezGn <alvarezguzman.dh@gmail.com>
2022-03-22 21:16:15 -0600

    add: Recipes Fragment

---
+++ app/src/main/java/com/nutrikares/nutrideskapp/ui/recipes/RecipesFragment.kt
@@ -0,0 +1,46 @@
+package com.nutrikares.nutrideskapp.ui.recipes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.nutrikares.nutrideskapp.R
+import com.nutrikares.nutrideskapp.databinding.FragmentPacientsBinding
+import com.nutrikares.nutrideskapp.databinding.FragmentRecipesBinding
+
+
+class RecipesFragment : Fragment() {
+
+    private var _binding: FragmentRecipesBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textRecipes
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        //_binding = null
+    }
+
+
+}
