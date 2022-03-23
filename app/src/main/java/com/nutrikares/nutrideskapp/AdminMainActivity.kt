1edb96ae2e9b256b16f5b695b8800aa85a24ddb3
DiegoAlvarezGn <alvarezguzman.dh@gmail.com>
2022-03-22 23:49:12 -0600

    Modify : There are only three items in the menu

--- app/src/main/java/com/nutrikares/nutrideskapp/AdminMainActivity.kt
+++ app/src/main/java/com/nutrikares/nutrideskapp/AdminMainActivity.kt
@@ -37,7 +37,7 @@ class AdminMainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_pacients, R.id.nav_recipes, R.id.nav_routines
+                R.id.nav_pacients, R.id.nav_recipes, R.id.nav_routines
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
