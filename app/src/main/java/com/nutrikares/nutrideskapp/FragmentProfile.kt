943071eb0046e8b4b08f4b2d0f7a92e55e5abc77
Josué Reyes <josue.hreyess@gmail.com>
2022-04-21 17:59:09 -0500

    Updates user password

--- app/src/main/java/com/nutrikares/nutrideskapp/FragmentProfile.kt
+++ app/src/main/java/com/nutrikares/nutrideskapp/FragmentProfile.kt
@@ -6,6 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.EmailAuthCredential
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.nutrikares.nutrideskapp.data.Datasource
@@ -33,6 +36,33 @@ class FragmentProfile : Fragment() {
             val intent = Intent(activity, SignIn::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
+        }
+        binding.saveBtn.setOnClickListener {
+            val user = Firebase.auth.currentUser!!
+            val currentPassword = binding.currentPassword.editText!!.text.toString()
+            val newPassword = binding.newPassword.editText!!.text.toString()
+            val confirmPassword = binding.confirmPassword.editText!!.text.toString()
+            if(newPassword.trim() == confirmPassword.trim()){
+                val credential = EmailAuthProvider.getCredential(user.email!!, currentPassword)
+                user.reauthenticate(credential).addOnCompleteListener {
+                    if(it.isSuccessful){
+                        user.updatePassword(newPassword).addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(activity, "Contraseña actualizada exitosamente", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(activity, SignIn::class.java)
+                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                                startActivity(intent)
+                            }else{
+                                Toast.makeText(activity, "Algo salió mal! Cambios no aplicados", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }else{
+                        Toast.makeText(activity, "Contraseña actual inválida", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(activity, "Contraseñas no coinciden", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
